5eb6470267c1947b01479e9ac3563ac3a85285e5
cozyns <cozyns008@gmail.com>
2026-03-28 09:15:50 +0000

    Deploy: 2026-03-28 09:15

--- assets/js/fetchHeader.js
+++ assets/js/fetchHeader.js
@@ -1,4 +1,4 @@
-fetch('/header/header.html')
+fetch('assets/header/header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header-placeholder').innerHTML = data;
